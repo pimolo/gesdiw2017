da2a1088bea4b87ef8f3e76ff7c15bb9fd7b8d74
Rémi Andrieux <r.andrieux@scoringline.com>
2017-04-27 23:56:04 +0200

    Integrate offers and tests with React

--- resources/js/lib/recruitment/ui/MyProfile.js
+++ resources/js/lib/recruitment/ui/MyProfile.js
@@ -8,7 +8,61 @@ export default class MyProfile extends React.PureComponent {
 
     render () {
         return (
-            <h2>My Profile view</h2>
+            <main>
+                <h2 className="text-uppercase text-align-center">Mon compte</h2>
+                <form action="">
+                    <fieldset>
+                        <legend>Identité</legend>
+                        <p>
+                            <label for="civilite">Civilité : </label>
+                            <label for="m"><input type="radio" name="civilite" value="m" id="m"></input> Homme</label>
+                            <label for="f"><input type="radio" name="civilite" value="f" id="f"></input> Femme</label>
+                            <label for="o"><input type="radio" name="civilite" value="o" id="o"></input> Autre</label>
+                        </p>
+                        <p>
+                            <label for="lastname">Nom : <input type="text" id="lastname" name="lastname"></input></label>
+                        </p>
+                        <p>
+                            <label for="firstname">Prénom : <input type="text" id="firstname" name="firstname"></input></label>
+                        </p>
+                    </fieldset>
+                    <fieldset>
+                        <legend>Informations complémentaires</legend>
+                        <p>
+                            <label for="zipcode">Code postal : <input type="text" id="zipcode" name="zipcode"></input></label>
+                            <label for="city">Ville : <input type="text" id="city" name="city"></input></label>
+                        </p>
+                        <p>
+                            <label for="phone">Téléphone : <input type="text" id="phone" name="phone"></input></label>
+                        </p>
+                    </fieldset>
+                    <fieldset>
+                        <legend>Formation</legend>
+                        <p>
+                            <label for="cursus">Niveau d'études : <input type="text" id="cursus" name="cursus"></input></label>
+                        </p>
+                        <p>
+                            <label for="grade">Diplôme : <input type="text" id="grade" name="grade"></input></label>
+                        </p>
+                    </fieldset>
+                    <fieldset>
+                        <legend>Pièces jointes</legend>
+                        <p>
+                            <label for="cv">Mon CV : <input type="file" id="cv"></input></label>
+                        </p>
+                    </fieldset>
+                    <fieldset>
+                        <legend>Mot de passe</legend>
+                        <p>
+                            <label for="password">Nouveau mot de passe : <input type="password" id="password" name="password"></input></label>
+                        </p>
+                        <p>
+                            <label for="confirm-password">Confirmation mot de passe : <input type="password" id="confirm-password" name="confirm-password"></input></label>
+                        </p>
+                    </fieldset>
+                    <p><input className="submit-button" type="submit"></input></p>
+                </form>
+            </main>
         );
     }
 }
--- resources/js/lib/recruitment/ui/MyTests.js
+++ resources/js/lib/recruitment/ui/MyTests.js
@@ -1,6 +1,29 @@
 import React from "react";
 import PropTypes from 'prop-types';
 
+const styles = {
+    selectDomain: {
+        display: 'block',
+        margin: '0 auto'
+    },
+    buttonEnabled: {
+        color: 'white',
+        textTransform: 'uppercase',
+        width: '14vw',
+        margin: '0 auto',
+        padding: '5px',
+        backgroundColor: 'orange'
+    },
+    buttonDisabled: {
+        color: 'white',
+        textTransform: 'uppercase',
+        width: '14vw',
+        margin: '0 auto',
+        padding: '5px',
+        backgroundColor: 'grey'
+    }
+}
+
 export default class MyTests extends React.PureComponent {
     constructor(props) {
         super(props);
@@ -8,7 +31,34 @@ export default class MyTests extends React.PureComponent {
 
     render () {
         return (
-            <h2>My Tests view</h2>
+            <main>
+                <h2 className="text-uppercase text-align-center">Mes tests</h2>
+                <p className="text-uppercase text-align-center">Sélectionner un domaine d'activité</p>
+                <select style={styles.selectDomain} className="select-domain" name="domain" id="domain">
+                    <option value="1">Développement</option>
+                </select>
+                <h3 className="text-uppercase">Tests disponibles</h3>
+                <div className="test-card text-align-center">
+                    <h4 className="test-card-header text-uppercase">
+                        Test PHP5
+                    </h4>
+                    <p>Mettez vos compétences à l'épreuve en lançant un test PHP5.</p>
+                    <div className="test-card-footer">
+                        <p>Etat : <span className="text-invalid">Non validé</span></p>
+                        <div style={styles.buttonEnabled} className="launch-test-button">Lancer le test</div>
+                    </div>
+                </div>
+                <div className="test-card text-align-center">
+                    <h4 className="test-card-header text-uppercase">
+                        Test Java
+                    </h4>
+                    <p>Mettez vos compétences à l'épreuve en lançant un test Java.</p>
+                    <div className="test-card-footer">
+                        <p>Etat : <span className="text-valid">Validé</span></p>
+                        <div style={styles.buttonDisabled} className="launch-test-button">Lancer le test</div>
+                    </div>
+                </div>
+            </main>
         );
     }
 }
