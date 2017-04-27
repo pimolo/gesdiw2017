7779a27fc14ef7226d1aefceae02e69ef5dcc4df
dydyFos <dylanfoster.esgi@gmail.com>
2017-04-27 22:34:36 +0200

    failed attempt to merge offers in react

--- resources/js/lib/recruitment/ui/Offers.js
+++ resources/js/lib/recruitment/ui/Offers.js
@@ -10,30 +10,23 @@ export default class Offers extends React.PureComponent {
 
     render () {
         return (
-            <main>
-                <div className="tape"></div>
-                <h2 className="uppercase text-center">Mes offres en cours</h2>
+            <main className="card">
+                <h2 className="uppercase text-center">Mes offres</h2>
                 <h3 className="uppercase">Mes candidatures</h3>
-                <div className="text-center">
-                    <p className="offer-element uppercase">poste</p>
-                    <hr />
+                <article className="text-center">
+                    <p className="test-card-header uppercase">poste</p>
                     <p className="offer-element">address</p>
                     <p className="offer-element">Contrat : <span className="font-bold">CDI</span></p>
                     <p className="offer-element">tests</p>
-                </div>
-                <article className="text-center">
-                    <p className="offer-element uppercase">poste</p>
-                    <hr />
-                    <p className="offer-element">address</p>
-                    <p className="offer-element">Contrat : <span className="bold">CDI</span></p>
-                    <div className="offer-element">test php :
-                        <span className="text-valid">validé</span>
-                          <br />
-                          test java : <span className="text-invalid">Non validé</span>
-                    </div>
                 </article>
-                <h3 className="text-uppercase">Autres offres dans mon domaine</h3>
-                <table className="text-uppercase">
+                <article className="text-center">
+                    <p className="test-card-header uppercase">poste</p>
+                    <p className="offer-element">address</p>
+                    <p className="offer-element">Contrat : <span className="font-bold">CDI</span></p>
+                    <p className="offer-element">test java : <span className="text-invalid">Non validé</span></p>
+                </article>
+                <h3 className="uppercase">Autres offres dans mon domaine</h3>
+                <table className="uppercase">
                     <thead>
                         <tr className="head-row">
                             <th>Offres</th>
@@ -57,7 +50,7 @@ export default class Offers extends React.PureComponent {
                         </tr>
                     </tbody>
                 </table>
-            </main>
+          </main>
         );
     }
 }
