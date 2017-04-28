fbcc892a8bdd43a0e560b9297c18d09d9de9f1e4
dydyFos <dylanfoster.esgi@gmail.com>
2017-04-28 06:27:38 +0200

    minimum information displaid in modal

--- resources/js/lib/recruitment/ui/OfferModal.js
+++ resources/js/lib/recruitment/ui/OfferModal.js
@@ -5,6 +5,15 @@ import Dialog from 'material-ui/Dialog';
 import RaisedButton from 'material-ui/RaisedButton';
 import FlatButton from 'material-ui/FlatButton';
 
+const styles = {
+    subContainer: {
+        marginTop: "30px"
+    },
+    parag: {
+        marginTop: "15px"
+    }
+}
+
 export default class OfferModal extends React.PureComponent {
     constructor(props) {
         super(props);
@@ -25,18 +34,29 @@ export default class OfferModal extends React.PureComponent {
                 />,
             ];
             return (
-                <div>
-                    <RaisedButton label="Scrollable Dialog"/>
-                    <Dialog
-                        title={offer.title}
-                        actions={actions}
-                        modal={false}
-                        open={offer !== false}
-                        autoScrollBodyContent={true}
-                        onRequestClose={this.props.hide_offer}
-                    >
-                    </Dialog>
-                </div>
+                <Dialog
+                    title={offer.title + " - " + offer.contractType}
+                    actions={actions}
+                    modal={false}
+                    open={offer !== false}
+                    autoScrollBodyContent={true}
+                    onRequestClose={this.props.hide_offer}
+                >
+                    <div className="display-flex-column full-width space-between">
+                        <div style={styles.subContainer} className="display-flex-column space-between full-width">
+                            <p className="full-width text-left title-3 font-bold">Mission</p>
+                            <p style={styles.parag} >{offer.description.mission || "Aucune description à ce jour"}</p>
+                        </div>
+                        <div style={styles.subContainer} className="display-flex-column space-between full-width">
+                            <p className="full-width text-left title-3 font-bold">Profile recherché</p>
+                            <p style={styles.parag} >{offer.description.profile || "Aucune description à ce jour"}</p>
+                        </div>
+                        <div style={styles.subContainer} className="display-flex-column space-between full-width">
+                            <p className="full-width text-left title-3 font-bold">Compétences requises</p>
+                            <p className="full-width text-left" style={styles.parag} >{offer.qualifications.map( o => o.name ).join(', ') || "Aucune description à ce jour"}</p>
+                        </div>
+                    </div>
+                </Dialog>
             );
         } else
             return null;
